eb87db6e6a98725282ffb0cb3ce5c7620257697e
Abhijeet Parida <abhijeet.parida@tum.de>
2024-11-06 09:47:57 -0500

    Create __init__.py

---
+++ code/hail/__init__.py
@@ -0,0 +1,23 @@
+"""
+### API Documentation for Pediatric MR-T1 Harmonizer
+
+Welcome to the API documentation for the code. Navigate through the table of contents on the left to explore detailed documentation for each submodule.
+
+This project develops the **Pediatric Brain MR-T1 Harmonization**, a free, open-source web-based application
+designed at [Children's National Hospital](https://www.childrensnational.org/) as part of the [HOPE platform](https://docs.hope4kids.io/HOPE-Platform/) 
+for the segmentation and analysis of pediatric brain tumors in magnetic resonance imaging (MRI). Developed in Python, this software aims to provide tools for harmonizing pediatric brain MRI, to support clinical decision-making in diagnosis and prognosis.  The software and instructions are available at [hail.hope4kids.io](https://hail.hope4kids.io/) and [instructions](https://docs.hope4kids.io/SegmenterApp-HAIL/).
+
+<img alt="Example Screenshot" src="https://raw.githubusercontent.com/Precision-Medical-Imaging-Group/SegmenterApp-HAIL/refs/heads/main/docs/assets/img/HarmonizationExample.png"  width="500">
+
+
+### Citations
+
+If you use and/or refer to this software in your research, please cite the following papers: 
+
+* Parida, Abhijeet, Zhifan Jiang, Syed Muhammad Anwar, Nicholas Foreman, Nicholas Stence, Michael J. Fisher, Roger J. Packer, Robert A. Avery, and Marius George Linguraru. "Harmonization Across Imaging Locations (HAIL): One-Shot Learning for Brain MRI." arXiv preprint arXiv:2308.11047 (2023).
+
+### Disclaimer
+
+This software is provided without any warranties or liabilities and is intended for research purposes only. It has not been reviewed or approved for clinical use by the Food and Drug Administration (FDA) or any other federal or state agency. 
+
+"""
